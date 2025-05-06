277b125e6ecd74e96ddfb0ef322a33c7ebe0618e
Ravi Kumar Bharti <43597318+rkbharti@users.noreply.github.com>
2025-05-06 08:27:28 +0000

    POSTFIX AND PREFIX BY INCREMENT AND DECREMENT

--- 01_basic/03_conversionOperations.js
+++ 01_basic/03_conversionOperations.js
@@ -33,7 +33,7 @@ console.log(typeof booleanIsLoggedIn);
 
 //third conversion is number type to String 
 
-let someNumber = 100
+/* let someNumber = 100
 
 console.log(someNumber);
 console.log(typeof someNumber);
@@ -44,3 +44,48 @@ let someNumberToString =  String(someNumber)
 
 console.log(someNumberToString);
 console.log(typeof someNumberToString);
+*/
+
+ //*******************************OPERATIONS************************** */
+   
+ // positive to negative
+  /* let value = 3
+ let negValueNew = -value
+ console.log(negValueNew);
+ */
+
+ /*   INCREMENT AND DECREMENT FOR POSTFIX AND PREFIX */
+/*
+let sampleNumber = "23"; 
+const digitNumbber = Number(sampleNumber) // change from string to number
+console.log(typeof digitNumbber);
+const postfix1 = sampleNumber++;
+console.log('*****************************postfix for Increment and Decremnt*******************************');
+
+console.log('postfix for this number in increment');
+console.log(`sampleNumber:${sampleNumber}, postfix1:${postfix1}`);
+console.log(sampleNumber)
+const postfix = sampleNumber--; //24 aftre updation
+console.log('postfix for this number in decrement ');
+console.log(`sampleNumber:${sampleNumber}, postfix:${postfix}`);
+console.log(sampleNumber);
+
+
+
+
+
+let sampleNumberforprefix = 50;
+
+const prefix2 = ++sampleNumberforprefix
+console.log('*************************PREFIX for increment and decrement***************************************');
+
+console.log('prefix2 for this number is increment');
+console.log(`sampleNumberforprefix:${sampleNumberforprefix}, prefix2:${prefix2}`)
+console.log(sampleNumberforprefix) //51 after updation 
+
+console.log('prefix ffor this number is decrement');
+const prefix = --sampleNumberforprefix
+console.log(`sampleNumberforprefix:${sampleNumberforprefix}, prefix:${prefix}`)
+console.log(sampleNumberforprefix);
+
+*/
